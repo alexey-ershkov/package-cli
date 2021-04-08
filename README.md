854eadbe9ce824234b8370c9551be6e77c332536
alexey ershkov <ershkov.alex2000@gmail.com>
2021-04-08 11:37:18 +0300

    minor fix

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # package-cli
-A CLI for looking and add dependencies to package.json
+A CLI to show and add dependencies to package.json
+
+##Install
+Via npm
+```
+$ npm i -g mail-package-cli
+```
+
+## Usage
+Via npm
+```
+$ mail-package-cli
+```
+Via npx
+```
+$ npx mail-package-cli
+```
--- src/utils/getPackageJSON.ts
+++ src/utils/getPackageJSON.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default function GetPackageJSON(path_to_file: string = '.'): PackageJson|undefined {
     const filepath = path.join(process.cwd(),path_to_file, 'package.json');
-    console.log(filepath)
+
     if (!fs.existsSync(filepath)) {
         return undefined
     }
